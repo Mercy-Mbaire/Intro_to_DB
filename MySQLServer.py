a2a7f871022f8abb6db78a94b1edce15e7b67657
Mercy-Mbaire <mercymbaireh@gmail.com>
2025-12-16 01:48:20 +0300

    Update the except block to explicitly reference mysql.connector.Error

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
@@ -8,18 +7,18 @@ def create_database():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="mydevelopersql@26"  # add your MySQL password if needed
+            password="YOUR_MYSQL_PASSWORD"
         )
 
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
-        if connection and connection.is_connected():
+        if connection:
             cursor.close()
             connection.close()
 
